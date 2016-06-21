fc528cf11f317f7ac2ccd50fe6a7f6031572ea8f
Open62541 travis-ci <open62541-travis-ci@users.noreply.github.com>
2016-06-21 15:21:13 +0000

    updated generated documentation on webpage by travis-ci [ci skip]

---
+++ doc/current/CMakeFiles/doc_latex.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/doc_latex"
+  "../doc_src/types.rst"
+  "../doc_src/constants.rst"
+  "../doc_src/types_generated.rst"
+  "../doc_src/server.rst"
+  "../doc_src/client.rst"
+  "../doc_src/client_highlevel.rst"
+  "../doc_src/log.rst"
+  "../doc_src/connection.rst"
+  "../doc_src/services.rst"
+  "../doc_src/nodestore.rst"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/doc_latex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
