b309b3889acb0acfe39dda45413bb9ab025fbf67
Open62541 travis-ci <open62541-travis-ci@users.noreply.github.com>
2016-09-12 16:20:43 +0000

    updated generated documentation on webpage by travis-ci [ci skip]

---
+++ doc/current/CMakeFiles/doc_pdf.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/doc_pdf"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/doc_pdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
