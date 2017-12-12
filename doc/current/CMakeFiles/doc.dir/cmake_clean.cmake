208ff2f54ecf5a72e47826de118b7dc5d2a83a9c
Open62541 travis-ci <open62541-travis-ci@users.noreply.github.com>
2017-12-12 18:34:44 +0000

    updated generated documentation on webpage by travis-ci [ci skip]

--- doc/current/CMakeFiles/doc.dir/cmake_clean.cmake
+++ doc/current/CMakeFiles/doc.dir/cmake_clean.cmake
@@ -22,6 +22,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang)
+foreach(lang )
   include(CMakeFiles/doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- doc/current/CMakeFiles/doc_latex.dir/DependInfo.cmake
+++ doc/current/CMakeFiles/doc_latex.dir/DependInfo.cmake
@@ -3,23 +3,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS
-  "UA_NO_AMALGAMATION"
-  )
-
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../include"
-  "../plugins"
-  "../deps"
-  "."
-  "src_generated"
-  )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
